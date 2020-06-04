baab8a2127e7296b0a1b3e5f282dade11346fd9c
martinstadelmann <m.stadelmann@iqual.ch>
2020-06-04 16:18:47 +0200

    Added user predelete hook and event

--- src/MauticEvents.php
+++ src/MauticEvents.php
@@ -31,4 +31,9 @@ final class MauticEvents {
    * Name of the event fired after a user profile is updated.
    */
   const USER_PROFILE_UPDATE = 'iq_group.userProfileUpdate';
+
+  /**
+   * Name of the event fired before a user profile is deleted.
+   */
+  const USER_PROFILE_DELETE = 'iq_group.userProfileDelete';
 }
